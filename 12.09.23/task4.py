a5365290bab76d3cc90ad4f7848b0fad6ae805a6
dm-vev <80632449+dm-vev@users.noreply.github.com>
2023-09-11 20:44:55 +0300

    задание 4, стр 15

---
+++ 12.09.23/task4.py
@@ -0,0 +1,24 @@
+def get_month() -> int:
+    while True:
+        try:
+            month = int(input('Введите номер месяца: '))
+            if 1 <= month <= 12:
+                return month
+            else:
+                print('Номер месяца должен быть от 1 до 12')
+        except ValueError:
+            print('Введено неверное значение')
+            
+def get_season(month: int):
+    if 3 <= month <= 5:
+        print('Весна')
+    elif 6 <= month <= 8:
+        print('Лето')
+    elif 9 <= month <= 11:
+        print('Осень')
+    elif month == 12 or month == 1 or month == 2:
+        print('Зима')
+    else:
+        print('Ошибка в номере месяца')
+
+get_season(get_month())
